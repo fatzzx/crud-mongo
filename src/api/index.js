838da9dab27b9c0aa277fc5d989cb142029c6056
Felipe Farias <felipefariasmac@MacBook-Pro-de-Felipe.local>
2025-05-01 12:03:06 -0300

    primeiro model

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import db from "../database/configdb.js";
+import User from "../models/User.js";
 
 dotenv.config();
+db.connect();
 
 const app = express();
 
