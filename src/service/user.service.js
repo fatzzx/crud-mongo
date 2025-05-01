6db998c16d92ebb8c4231e66ab61de174cd10912
Felipe Farias <felipefariasmac@MacBook-Pro-de-Felipe.local>
2025-05-01 18:06:49 -0300

    autenticacao

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 const register = async (req, res) => {
   if (!req.body.username || !req.body.password) {
@@ -29,4 +30,36 @@ const register = async (req, res) => {
   }
 };
 
-export default { register };
+const login = async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    return res
+      .status(400)
+      .json({ message: "Username and password are required" });
+  }
+
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username }).select("+password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({
+      message: `User logged in successfully -> ${user.username}`,
+      token,
+    });
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export default { register, login };
